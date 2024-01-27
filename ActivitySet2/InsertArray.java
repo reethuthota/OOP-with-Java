6bfff90085676a9eb74e3f9f9dd86cebaa6f534e
reethuthota <reethu.thota@gmail.com>
2024-01-27 15:21:09 +0530

    Update InsertArray.java

--- ActivitySet2/InsertArray.java
+++ ActivitySet2/InsertArray.java
@@ -1,27 +1,42 @@
-// Java Programming Code to Insert Element in Array
+//Insert Element in a given position in an array
 
 import java.util.Scanner;
 
 public class InsertArray {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter size of array : ");
-        int size = myObj.nextInt();
+        int size = scanner.nextInt();
         int myArray[] = new int[size];
 
         for(int i=0; i<size; i++) {
             System.out.print("Enter element [" + (i+1) + "] : ");
-            int ele = myObj.nextInt();
-            myArray[i] = ele;
-        }
-        
-        System.out.print("Array elements : ");
-        for(int i=0; i<size; i++) {
-            System.out.print(myArray[i]);
+            myArray[i] = scanner.nextInt();
         }
 
-        myObj.close();
+        System.out.print("Enter new element to be inserted : ");
+        int newElement = scanner.nextInt();
+        System.out.print("Enter position to insert element at (stating from 1): ");
+        int position = scanner.nextInt();
+
+        if(position<0 || position>size){
+            System.out.println("Position entered is invalid!");
+        } else {
+            int newArray[] = new int[size+1];
+            for(int i=0; i<position-1; i++){
+                newArray[i] = myArray[i];
+            }
+            newArray[position-1]=newElement;
+            for(int i=position; i<=size; i++){
+                newArray[i] = myArray[i-1];
+            }
+            
+            System.out.print("Array elements : ");
+            for(int i=0; i<size+1; i++) {
+                System.out.print(newArray[i]);
+            }
+        }
+        scanner.close();
     }
 }
