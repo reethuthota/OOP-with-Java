855b7b3f608f1de80f2419e756569f3cf108d00b
reethuthota <reethu.thota@gmail.com>
2024-02-08 14:11:10 +0530

    Create CO3

---
+++ CO3/Method_Overloading/Q2_MO.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Addition
+{
+    void add(int a, int b)
+    {
+        System.out.println("The sum of two integer values: "+(a+b));
+    }
+
+    void add(int a, int b, float x)
+    {
+        System.out.println("The sum of two integer values and a float value: "+(a+b+x));
+    }
+
+    void add(int i, int j , int k)
+    {
+        System.out.println("The sum of three integer values: "+(i+j+k));
+    }
+}
+public class Q2_MO {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two integer values: ");
+        Addition ob=new Addition();
+        ob.add(sc.nextInt(),sc.nextInt());
+
+        System.out.println("Enter two integer values and a float value: ");
+        ob.add(sc.nextInt(),sc.nextInt(),sc.nextFloat());
+
+        System.out.println("Enter three integer values: " );
+        ob.add(sc.nextInt(),sc.nextInt(),sc.nextInt());
+    }
+}
