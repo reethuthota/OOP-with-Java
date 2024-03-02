ca5e7ba32ed17c38a34e9cc340a627d4d3ee0879
reethuthota <reethu.thota@gmail.com>
2024-03-02 14:35:54 +0530

    Update q1.java

--- CO3/Worksheet4/q1.java
+++ CO3/Worksheet4/q1.java
@@ -1,3 +1,5 @@
+//Create an abstract class "Parent" with a method "message". It has two subclasses each having a method with the same name "message" that prints "This is first subclass" and "This is second subclass" respectively. Call the methods "message" by creating an object for each subclass.
+
 package Worksheet4;
 
 abstract class Parent {
